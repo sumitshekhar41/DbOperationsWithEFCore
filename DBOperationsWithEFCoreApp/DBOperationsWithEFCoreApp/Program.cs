f83064a4dd6efb951cbc039064dbfecad76a328a
sumitshekhar41 <sumit.shekhar41@gmail.com>
2026-03-26 09:56:48 +0530

    Install & initialize EF Core

--- DBOperationsWithEFCoreApp/DBOperationsWithEFCoreApp/Program.cs
+++ DBOperationsWithEFCoreApp/DBOperationsWithEFCoreApp/Program.cs
@@ -1,4 +1,10 @@
+using DBOperationsWithEFCoreApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
